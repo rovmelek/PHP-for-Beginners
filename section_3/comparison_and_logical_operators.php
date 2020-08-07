93f31dbcf4cfad8727c1036f6576563a8cbcf3d7
Kevin Tseng <hytseng@TRPG.org>
2020-08-07 14:08:53 -0700

    section 3.16

---
+++ section_3/comparison_and_logical_operators.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="https://www.w3schools.com/php/php_operators.asp">w3schools: PHP Operators</a>
+    <br>
+    <a href="https://www.php.net/manual/en/language.operators.php">PHP manual: PHP Operators</a>
+    <?php  ?>
+</body>
+</html>
