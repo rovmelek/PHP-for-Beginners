f23434cbf94c521814c8dd971d1cb45e92d3fc38
Kevin Tseng <hytseng@TRPG.org>
2020-08-07 11:45:59 -0700

    section 2.12 arrays

---
+++ section_2/arrays.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$numberList = [1, 2, 'abc'];
+// newer way to declare/assign array
+// $numberList = [1, 2, 'abc'];
+echo $numberList[2];
+
+echo '<br>';
+
+print_r($numberList);
+?>
+</body>
+</html>
