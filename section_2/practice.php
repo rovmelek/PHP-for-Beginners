c42d15b3377e6ef99bc8f8a7332c6cdd3541552a
Kevin Tseng <hytseng@TRPG.org>
2020-08-07 13:45:52 -0700

    section 2 practice

---
+++ section_2/practice.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$number1 = 10;
+$number2 = 20;
+
+echo $number1 + $number2 . '<br>';
+
+$array1 = [1, 2, 3];
+$array2 = ['number1' => 1, 'number2' => 2, 'number3' => 3];
+
+print_r($array1);
+echo '<br>';
+print_r($array2);
+?>
+</body>
+</html>
