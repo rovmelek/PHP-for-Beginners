57f66da7f1fce0bf139b12a2b37f65967a0084e6
Kevin Tseng <hytseng@TRPG.org>
2020-08-17 13:09:36 -0700

    5.32 practice

--- _practical-app/5.php
+++ _practical-app/5.php
@@ -1,36 +1,24 @@
 <?php include "functions.php"; ?>
-<?php include "includes/header.php";?>
+<?php include "includes/header.php"; ?>
 	<section class="content">
 
 		<aside class="col-xs-4">
-		<?php Navigation();?>
+		<?php Navigation(); ?>
 			
 			
 		</aside><!--SIDEBAR-->
 
 
 <article class="main-content col-xs-8">
-
 	
-	<?php 
-
-
-/*  Step1: Use a pre-built math function here and echo it
-
-
-	Step 2:  Use a pre-built string function here and echo it
-
-
-	Step 3:  Use a pre-built Array function here and echo it
-
- */
-
-	
+<?php
+$round = round(4.5);
+echo "round of 4.5 is $round<br>";
+$helloWorld = strtolower('Hello World!');
+echo $helloWorld . '<br>';
+$myArr = [1, 2, 3, 4, 5];
+echo 'Min of [1, 2, 3, 4, 5] is ' . min($myArr) . '<br>';
 ?>
-
-
-
-
 
 </article><!--MAIN CONTENT-->
 <?php include "includes/footer.php"; ?>
