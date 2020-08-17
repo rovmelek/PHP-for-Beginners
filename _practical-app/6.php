f57c863d3c59b73b0f93ffd489c16e5b23561f64
Kevin Tseng <hytseng@TRPG.org>
2020-08-17 14:05:57 -0700

    6.37 practice

--- _practical-app/6.php
+++ _practical-app/6.php
@@ -1,29 +1,16 @@
 
 <?php include "functions.php"; ?>
-<?php include "includes/header.php";?>
+<?php include "includes/header.php"; ?>
 
 	<section class="content">
 
 		<aside class="col-xs-4">
 		
-		<?php Navigation();?>
+		<?php Navigation(); ?>
 			
 		</aside><!--SIDEBAR-->
 
 
 <article class="main-content col-xs-8">
- 
-
-	<?php  
-
-/*  Step1: Make a form that submits one value to POST super global
-
-
- */
-
-	
-?>
-
-
 </article><!--MAIN CONTENT-->
 <?php include "includes/footer.php"; ?>
---
+++ _practical-app/section_6/practice.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php if (isset($_POST['submit'])) {
+      echo 'User input text is: ' . $_POST['textInput'];
+    } ?>
+    <form action="" method="post">
+    <label for="textInput">Enter text:</label>
+    <input type="text" id="textInput" name="textInput">
+    <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
